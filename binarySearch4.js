69014281263ae68f17725db0b62ea141abd7eb44
derin <derinbino@gmail.com>
2022-10-21 19:31:28 -0500

    practicing binary search

---
+++ binarySearch4.js
@@ -0,0 +1,20 @@
+function binarySearch(array,target){
+    let left = 0;
+    let right = array.length -1;
+
+    while(left <= right){
+        let mid = Math.floor((left + right)/2);
+
+        if(target == array[mid]){
+            return mid;
+        }else if (target < array[mid]){
+            right = mid - 1;
+        }else{
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+
+
+console.log("expect 7: ", binarySearch([1,2,3,4,5,6,7,8,9,10,11,12], 8));
